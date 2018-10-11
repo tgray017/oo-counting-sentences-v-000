5dc25c5cd9d48eb9ecd2354b8f6a221936ca3104
tgray017 <tgray017@gmail.com>
2018-10-11 13:26:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.split(/\.|!|\?/)
+    sentence_array.reject! {|sentence| sentence.empty?}
+    sentence_array.count
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
